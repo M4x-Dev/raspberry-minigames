7e7aaa0f7918e3c92e384a53439a6050a8416c9e
Maximilian Flügel <fluegel@hypros.de>
2022-12-11 14:14:44 +0100

    Add tube generation

--- bin/flappy_bird.py
+++ bin/flappy_bird.py
@@ -14,8 +14,7 @@ sense = SenseHat()
 game_matrix = SenseMatrix()
 
 bird = Bird(1, 4, bird_color)
-tubes = []
-tubes.append(TubeSet(3, 6, tube_color))
+tubes = [TubeSet(randint(0, 5), 6, tube_color)]
 
 second_iteration = True
 while True:
@@ -29,10 +28,22 @@ while True:
     bird.apply_gravity()
 
     if second_iteration:
-        # Move all tubes
+        # Add new tubes
+        if tubes[-1].get_x() == 4:
+            tubes.append(TubeSet(randint(0, 5), 6, tube_color))
+
+        # Move all tubes and remove redundant ones
         second_iteration = False
+        tubes_to_remove = []
+
         for tube in tubes:
+            if tube.get_x() < 0:
+                tubes_to_remove.append(tube)
             tube.move(-1)
+
+        # Remove redundant tubes
+        for tube in tubes_to_remove:
+            tubes.remove(tube)
     else:
         # Update on next iteration
         second_iteration = True
--- src/flappy_bird/tube_set.py
+++ src/flappy_bird/tube_set.py
@@ -4,10 +4,13 @@ class TubeSet:
         self.__x_position = x_position
         self.__color = color
 
+    def get_x(self):
+        return self.__x_position
+
     def draw(self, matrix):
         for x in range(2):
-            for y in range(7):
-                if y < self.__outlet_height or y > self.__outlet_height + 1:
+            for y in range(8):
+                if y < self.__outlet_height or y > self.__outlet_height + 2:
                     if 0 <= self.__x_position + x <= 7 and 0 <= y <= 7:
                         matrix.set_pixel(self.__x_position + x, y, self.__color)
 
