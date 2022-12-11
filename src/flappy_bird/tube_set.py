9740ef36fff0112d33c18d003d306580b06b60bc
Maximilian Flügel <fluegel@hypros.de>
2022-12-11 14:03:44 +0100

    Add color to tubes

--- src/flappy_bird/tube_set.py
+++ src/flappy_bird/tube_set.py
@@ -9,7 +9,7 @@ class TubeSet:
             for y in range(7):
                 if y < self.__outlet_height or y > self.__outlet_height + 1:
                     if 0 <= self.__x_position + x <= 7 and 0 <= y <= 7:
-                        matrix.set_pixel(self.__x_position + x, y)
+                        matrix.set_pixel(self.__x_position + x, y, self.__color)
 
     def move(self, delta_x):
         self.__x_position += delta_x
