72875a966312b2bc10462f8ddaef244f599e67dc
Maximilian Flügel <fluegel@hypros.de>
2022-12-11 13:36:57 +0100

    Fix bug in collision detection

--- bin/snake.py
+++ bin/snake.py
@@ -33,7 +33,7 @@ while True:
 
     # Update the snake body
     snake.insert(0, [snake[0][0] + snake_velocity[0], snake[0][1] + snake_velocity[1]])
-    if snake[0] == food[0] and snake[1] == food[1]:
+    if snake[0][0] == food[0] and snake[0][1] == food[1]:
         food = [randint(0, 7), randint(0, 7)]
     else:
         snake.pop()
